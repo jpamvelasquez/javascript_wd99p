6b43f87f505e604dbd982033fd44e137f1413024
jpamvelasquez <caspajeve@gmail.com>
2024-03-25 19:51:19 +0800

    operations

---
+++ operations/script.js
@@ -0,0 +1,6 @@
+let num1 = prompt("Enter first Number");
+let num2 = prompt("Enter second Number");
+let total = parseFloat(num1) + parseFloat(num2);
+
+let display = `Total ${total}`;
+console.log(display);
